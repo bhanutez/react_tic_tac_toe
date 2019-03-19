0e2dc4620bccb052e8c9e38965bfccce9948636a
Bhanutv <bhanutv@IN-Bhanutv.local>
2019-03-19 15:46:04 +0530

    Change class component Square to function component

--- src/Square.js
+++ src/Square.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
 import './Style.css';
 
-class Square extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null
-        }
-    }
-
-    render() {
-        return (
-            <button className="square" onClick={ () => this.props.onClick()}>
-                {this.props.value}
+function Square(props) {
+    return (
+        <button className="square" onClick={ () => props.onClick()}>
+                {props.value}
             </button>
-        );
-    }
+    );
 }
 
 export default Square;
